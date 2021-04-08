eb8b76228058200b4b8c3c7774f30d55b8a4f304
domRG <22130302+domRG@users.noreply.github.com>
2021-04-08 19:41:52 +0100

    Working kukaOmni

--- python_v_rep/testcom.py
+++ python_v_rep/testcom.py
@@ -15,7 +15,8 @@ vrep.simxFinish(-1) # just in case, close all opened connections
 clientID=vrep.simxStart('127.0.0.1',19999,True,True,5000,5) # Connect to V-REP
 if clientID!=-1:
     print ('Connected to remote API server')
-    print(f'CLient ID: {clientID:d}')
+    print('CLient ID: '),
+    print(clientID)
     
 else:
     print ('Connection not succesful')
